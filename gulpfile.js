3e7cfb52a43601d331d497d9eea875a993fdcaaa
Nils Persson <nils@nedge.se>
2018-05-05 12:04:38 +0200

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+const gulp = require('gulp'),
+	del = require('del'),
+	pump = require('pump'),
+	babel = require('gulp-babel'),
+	uglify = require('gulp-uglify'),
+	sourcemaps = require('gulp-sourcemaps'),
+	jshint = require('gulp-jshint'),
+	sass = require('gulp-sass'),
+	cleanhtml = require('gulp-cleanhtml'),
+	ftp = require('vinyl-ftp')
+
+// Clear dist folder
+gulp.task('clear', () =>
+  del(['dist'])
+)
+
+// Copy static files to dist folder
+gulp.task('copy', () =>
+  gulp.src(['src/**/*', '!src/**/*.html', '!src/**/*.sass'])
+		.pipe(gulp.dest('dist'))
+)
+
+// Copy and compress HTML files
+gulp.task('html', () =>
+	gulp.src('src/**/*.html')
+		.pipe(cleanhtml())
+		.pipe(gulp.dest('dist'))
+)
+
+// Run scripts through JSHint
+gulp.task('jshint', () =>
+	gulp.src('src/**/*.js')
+		.pipe(jshint({
+      esversion: 6, // Prefer ES6
+      asi: true // Don't bother with semicolons
+    }))
+		.pipe(jshint.reporter('default'))
+)
+
+// Uglify scripts with sourcemaps
+gulp.task('scripts', ['jshint'], () =>
+	pump([
+		gulp.src('src/**/*.js'),
+    sourcemaps.init(),
+    babel({presets: ['env']}),
+    uglify(),
+    sourcemaps.write('.'),
+    gulp.dest('dist')
+	])
+)
+
+// Compile, minify and copy SASS
+gulp.task('styles', () =>
+	gulp.src('src/**/*.sass')
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}).on('error', sass.logError))
+		.pipe(gulp.dest('dist'))
+)
+
+const defaults = ['copy', 'html', 'scripts', 'styles']
+
+// Run all tasks after build directory has been cleaned
+gulp.task('default', ['clear'], () => {
+  gulp.start(defaults.concat('watch'))
+})
+
+// Magic watch task <3
+gulp.task('watch', () => {
+  gulp.watch(['src/**/*', '!src/**/*.html', '!src/**/*.sass'], ['copy'])
+  gulp.watch('src/**/*.html', ['html'])
+  gulp.watch('src/**/*.js', ['scripts'])
+  gulp.watch('src/**/*.sass', ['styles'])
+})
+
+// FTP Upload
+gulp.task('deploy', ['copy', 'html', 'scripts', 'styles'], () => {
+	const conn = ftp.create({
+		host: 'ftpcluster.loopia.se',
+		user: 'gourmetboulevarden.se',
+		password: 'rpaTDC6;,k4t'
+	})
+
+	const globs = ['dist/**/*', '!dist/**/*.map']
+	const options = {
+		base: 'dist',
+		buffer: false
+	}
+
+	return gulp.src(globs, options)
+		.pipe( conn.newer('/public_html') ) // only upload newer files
+		.pipe( conn.dest('/public_html') )
+})
